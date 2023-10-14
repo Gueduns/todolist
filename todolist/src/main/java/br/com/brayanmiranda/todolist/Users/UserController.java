6134f18ce70909e7d32c8b0194a7e7d26d741389
Brayan Mirada <rayanmelo.dev@gmail.com>
2023-10-14 18:29:06 -0300

    Second commit implementeção do UserRepository, conexão com o banco  e criação da tbela usuarios

--- todolist/src/main/java/br/com/brayanmiranda/todolist/Users/UserController.java
+++ todolist/src/main/java/br/com/brayanmiranda/todolist/Users/UserController.java
@@ -1,5 +1,8 @@
 package br.com.brayanmiranda.todolist.Users;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +12,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
+    //ResponseEntity
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        var user = this.userRepository.findByUsername(userModel.getUsername());
 
-        System.out.println(userModel.name);
+        if(user != null){
+            System.out.println("Usuário já existe");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+            //Mensagem de erro 
+            //Status Code  
+            //400 quando faz uma requisisão e não foi possivel continuar
+        }
 
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
     
 }
--- todolist/src/main/java/br/com/brayanmiranda/todolist/Users/UserModel.java
+++ todolist/src/main/java/br/com/brayanmiranda/todolist/Users/UserModel.java
@@ -1,9 +1,32 @@
 package br.com.brayanmiranda.todolist.Users;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_users")
 public class UserModel {
     
-    public String username;
-    public String name;
-    public String password;
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    //Caso queira que natabela a coluna tenha um nome diferente da aplicação
+    //@Column(name = "usuarios")
+    @Column(unique = true)
+    private String username;
+    private String name;
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 }
